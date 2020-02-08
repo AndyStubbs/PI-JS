67af4fd0c958263d138e34d0e904c447da74e97d
Andy Stubbs <astubbs50@gmail.com>
2020-02-07 20:33:34 -0800

    Adding paint command

---
+++ src/qbs-screen-graphics-paint.js
@@ -0,0 +1,76 @@
+/*
+* File: qbs-screen-graphics.js
+*/
+
+// Start of File Encapsulation
+( function () {
+
+var qbData;
+
+qbData = qbs._.data;
+
+qbs._.addCommand( "paint", paint, false, true, "both", "paint" );
+function paint( screenData, args ) {
+	var x, y, fc, fills, pixel, bc;
+
+	x = args[ 0 ];
+	y = args[ 1 ];
+	fc = args[ 2 ];
+
+	fills = [ {
+		"x": x,
+		"y": y
+	} ];
+
+	qbData.commands.getImageData( screenData );
+
+	// If the color is a number then get it from the pal
+	if( ! isNaN( fc ) ) {
+		fc = screenData.pal[ fc ];
+	} else if( ! fc || isNaN( fc.r ) || isNaN( fc.g ) || isNaN( fc.b ) ) {
+		fc = screenData.pal[ screenData.fColor ];
+	}
+
+	// Get the background color
+	bc = qbData.commands.getPixel( screenData, x, y );
+
+	// Loop until no fills left
+	while( fills.length > 0 ) {
+
+		pixel = fills.pop();
+
+		// Set the current pixel
+		qbData.commands.setPixel( screenData, pixel.x, pixel.y, fc );
+
+		// Add fills to neighbors
+		addFill( screenData, pixel.x + 1, pixel.y, fills, fc, bc );
+		addFill( screenData, pixel.x - 1, pixel.y, fills, fc, bc );
+		addFill( screenData, pixel.x, pixel.y + 1, fills, fc, bc );
+		addFill( screenData, pixel.x, pixel.y - 1, fills, fc, bc );
+	}
+
+	screenData.dirty = true;
+}
+
+function addFill( screenData, x, y, fills, fc, bc ) {
+	var fill;
+	if( floodCheck( screenData, x, y, fc, bc ) ) {
+		qbData.commands.setPixel( screenData, x, y, fc );
+		fill = { x: x, y: y };
+		fills.push( fill );
+	}
+}
+
+function floodCheck( screenData, x, y, fc, bc ) {
+	var pc;
+
+	if( x < 0 || x >= screenData.width || y < 0 || y >= screenData.height ) {
+		return false;
+	}
+	pc = qbData.commands.getPixel( screenData, x, y );
+	return ! qbs.util.compareColors( pc, fc ) && qbs.util.compareColors( pc, bc );
+}
+
+// End of File Encapsulation
+} )();
+
--- src/qbs-screen-helper.js
+++ src/qbs-screen-helper.js
@@ -1,9 +1,13 @@
 /*
-* File: qbs-print.js
+* File: qbs-screen-helper.js
 */
 
 // Start of File Encapsulation
 ( function () {
+
+var qbData;
+
+qbData = qbs._.data;
 
 qbs._.addCommand( "getImageData", getImageData, true, false );
 function getImageData( screenData ) {
@@ -37,6 +41,8 @@ function setPixelSafe( screenData, x, y, c ) {
 		return;
 	}
 
+	qbData.commands.getImageData( screenData );
+
 	// Get the image data
 	data = screenData.imageData.data;
 
@@ -48,6 +54,32 @@ function setPixelSafe( screenData, x, y, c ) {
 	data[ i + 2 ] = c.b;
 	data[ i + 3 ] = c.a;
 
+}
+
+qbs._.addCommand( "getPixel", getPixel, true, false );
+function getPixel( screenData, x, y ) {
+	var data, i;
+
+	// Get the image data
+	data = screenData.imageData.data;
+
+	// Calculate the index of the color
+	i = ( ( screenData.width * y ) + x ) * 4;
+
+	return {
+		r: data[i],
+		g: data[i + 1],
+		b: data[i + 2],
+		a: data[i + 3]
+	};
+}
+
+qbs._.addCommand( "getPixelSafe", getPixelSafe, true, false );
+function getPixelSafe( screenData, x, y ) {
+
+	qbData.commands.getImageData( screenData );
+
+	return getPixel( screenData, x, y );
 }
 
 // End of File Encapsulation
