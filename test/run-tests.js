f3abd3a492fb90bb43ff83a61eacbdc36d5be2ea
Andy Stubbs <astubbs50@gmail.com>
2020-04-30 12:16:36 -0700

    Refactored test script to be more efficient.

--- test/run-tests.js
+++ test/run-tests.js
@@ -1,228 +1,244 @@
-var cmd,
-	img_html,
-	str_html,
-	browser,
-	home,
-	png,
-	mis_match_count,
-	new_tests_count;
+"use strict";
 
-//Link to my local web browser
-browser = "\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\"";
-//browser = "firefox";
+// Libraries
+const CMD = require( "node-cmd" );
+const FS = require( "fs" );
+const PNG = require( "pngjs" ).PNG;
+const TOML = require( "@iarna/toml" );
 
-//Link to testing home page on local system
-home = "http://localhost:8080/test/index.html";
+// Global constants
+const BROWSER = "\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\"";
+const HOME = "http://localhost:8080/test/index.html";
+const TEMPLATE_FILE = "test/index-template.html";
+const INDEX_FILE = "test/index.html";
+//const LOG_FILE = "test/log_" + ( new Date() ).getTime() + ".log";
+const LOG_FILE = "test/log.log";
+const SCREENSHOTS_FOLDER = "test/tests/screenshots/";
+const TESTS_URL = "http://localhost:8080/test/tests/";
+const IMG_URL_ROOT = "tests/screenshots/";
+const TESTS_FOLDER = "test/tests";
+const ROOT_DIR = __dirname.substring( 0, __dirname.lastIndexOf( "\\" ) ) + "\\";
+const TEST_HTML_ID = "test_";
 
-//Import node cmd
-cmd = require( "node-cmd" );
+// Global variables
+let g_ImgHtml = [];
+let g_Errors = [];
+let g_MismatchCount = 0;
+let g_NewTestsCount = 0;
+let g_totalTestsCount = 0;
+let g_totalTestsParsedCount = 0;
+let g_StrHtml = FS.readFileSync( TEMPLATE_FILE ).toString();
+let g_Files = FS.readdirSync( TESTS_FOLDER );
+let g_StrLog = "";
 
-//Import the file system handler
-fs = require( "fs" );
-
-//Import PNG tool
-png = require( "pngjs" ).PNG;
-
-let templateFile = "test/index-template.html";
-let indexFile = "test/index.html";
-let screenshotsFolder = "test/tests/screenshots/";
-let testsUrl = "http://localhost:8080/test/tests/";
-let imgUrlRoot = "tests/screenshots/";
-let testsFolder = "test/tests";
-let root_dir = __dirname.substring(0, __dirname.lastIndexOf("\\")) + "\\";
-
-const toml = require("@iarna/toml");
-
-//Setup the img html
-img_html = "";
-
-//Setup counters
-mis_match_count = 0;
-new_tests_count = 0;
-
-str_html = fs.readFileSync(templateFile).toString();
-
-let files = fs.readdirSync(testsFolder);
-
-next_test(0);
-
-function getTestInfo(filename) {
-	let text = fs.readFileSync(testsFolder + "/" + filename).toString();
-	let tomlText = text.substring(text.indexOf("[[TOML_START]]") + 14, text.indexOf("[[TOML_END]]")).replace(/\t/g, "");
-	let data = toml.parse(tomlText);
-	return data;
+for( let i = 0; i < g_Files.length; i++ ) {
+	run_test( i );
 }
 
-function isHtmlFile(filename) {
-	let parts = filename.split(".");
-	if(parts.length < 2) {
-		return false;
-	}
-	return parts[1] === "html";
-}
+function run_test( i ) {
 
-function next_test( i ) {
-	"use strict";
+	let file = g_Files[ i ];
 
-	var test, cmd_str, save_file, stats_html;
-	
-	//Check if tests are completed
-	if( i >= files.length ) {
+	//Make the html look nice
+	g_ImgHtml.push( "\n\t" );
 
-		//Make the html look nice
-		img_html += "\n\t";
-
-		//Update the stats
-		stats_html = "";
-		if( mis_match_count === 0 && new_tests_count === 0 ) {
-			stats_html = "\n\t\t\t<span class='good'>All images match!</span>\n\t\t";
-		} else {
-			stats_html += "\n\t\t\t<span class='error'>Discrepancies Found</span>";
-			if( mis_match_count ) {
-				stats_html += "\n\t\t\t<span class='error'>" + mis_match_count +
-					" mismatched images.</span>";
-				if( ! new_tests_count ) {
-					stats_html += "\n\t\t";
-				}
-			}
-			if( new_tests_count ) {
-				stats_html += "\n\t\t\t<span class='neutral'>" + new_tests_count +
-					" new images not verified.</span>\n\t\t";
-			}
-		}
-		str_html = str_html.replace( "[TEST-STATS]", stats_html );
-
-		//Update the index_html
-		str_html = str_html.replace( "[WEB-TESTS]", img_html );
-
-		//Write the index.html file
-		fs.writeFile( indexFile, str_html, function () {
-			console.log( "Tests completed" );
-		} );
-
-		//Set the command to startup chrome and point to the home page
-		//cmd_str = "start /b \"" + browser + "\" \"" + home;
-		cmd_str = browser + " " + home;
-
-		//Launch Chrome with link to test file
-		cmd.get( cmd_str, function ( err, data, stderr ) {
-			if( err ) {
-				console.log( err );
-			}
-			if( data ) {
-				console.log( data );
-			}
-			if( stderr ) {
-				console.log( stderr );
-			}
-		} );
-
-		return;
-	}
-
-	let file = files[i];
-
-	if(!isHtmlFile(file)) {
-		next_test(i + 1);
+	if( ! isHtmlFile( file ) ) {
+		//next_test(i + 1);
 		return;
 	}
 
 	//Get current test
-	test = getTestInfo(file);
+	let test = getTestInfo( file );
 
 	//Set the name of the image file
-	test.img_file = screenshotsFolder + test.file + ".png";
+	test.img_file = SCREENSHOTS_FOLDER + test.file + ".png";
 
-	if( fs.existsSync( test.img_file ) ) {
-		save_file = screenshotsFolder + test.file + "_new.png";
-		test.new_img_file = save_file;
+	let saveFile = "";
+	if( FS.existsSync( test.img_file ) ) {
+		saveFile = SCREENSHOTS_FOLDER + test.file + "_new.png";
+		test.new_img_file = saveFile;
 	} else {
-		save_file = screenshotsFolder + test.file + ".png";
+		saveFile = SCREENSHOTS_FOLDER + test.file + ".png";
 		test.new_img_file = false;
 	}
 
 	//Set the test url
-	test.url = testsUrl + test.file + ".html";
+	test.url = TESTS_URL + test.file + ".html";
+	test.id = i;
 
 	//Update the image html
-	img_html += "\n\t\t<h2>" + test.name + "</h2>";
-	img_html += "\n\t\t<div class='link'><a href='" + test.url + "'>" + test.url + "</a></div>";
-	img_html += "\n\t\t[" + test.file + "]<br />";
-	img_html += "\n\t\t<img src='" + imgUrlRoot + test.file + ".png' />";
-	img_html += "\n\t\t<img src='" + imgUrlRoot + test.file + "_new.png' />";
+	g_ImgHtml[ i ] += "\n\t\t<div id='" + TEST_HTML_ID + i + "'></div>";
+	g_ImgHtml[ i ] += "\n\t\t<h2>" + test.name + "</h2>";
+	g_ImgHtml[ i ] += "\n\t\t<div class='link'><a href='" + test.url + "' target='_blank'>" + test.url + 
+		"</a>&nbsp;&nbsp;-&nbsp;&nbsp;<a href='#stats'>Go back</a></div>";
+	g_ImgHtml[ i ] += "\n\t\t[" + test.file + "]<br />";
+	g_ImgHtml[ i ] += "\n\t\t<img src='" + IMG_URL_ROOT + test.file + ".png' />";
+	g_ImgHtml[ i ] += "\n\t\t<img src='" + IMG_URL_ROOT + test.file + "_new.png' />";
 
-	//Set the command to run pegjs to create the basic parser
-	//cmd_str = casperCmd + test.test + " " + test.url + " " +
-	//	save_file + " " + test.width + " " + test.height + " " +
-	//	test.delay;
-	cmd_str = browser + " --headless --screenshot=" + root_dir + save_file + " --window-size=" + test.width + "," + test.height + " " + test.url;
+	let cmdStr = BROWSER + " --headless --screenshot=" + ROOT_DIR + saveFile + " --window-size=" + test.width + "," + test.height + " " + test.url;
 
 	//Log some output
 	console.log( "" );
 	console.log( "********************************************" );
 	console.log( test.name );
-	console.log( cmd_str );
+	console.log( cmdStr );
+
+	// Increment the count of totalTestsCounted
+	g_totalTestsCount += 1;
 
 	//run the command
-	cmd.get( cmd_str, function ( err, data, stderr ) {
+	CMD.get( cmdStr, function ( err, data, stderr ) {
 
-		var img1, img2, files_loaded, diff_rounded;
+		var img1, img2, filesLoaded, diffRounded;
 
 		function parsed() {
 			var diff;
 
-			files_loaded += 1;
-			if( files_loaded < 2 ) {
+			filesLoaded += 1;
+			if( filesLoaded < 2 ) {
 				return;
 			}
 
 			diff = compare_images( img1, img2 );
-			diff_rounded = Math.round( diff * 100 ) / 100;
+			diffRounded = Math.round( diff * 100 ) / 100;
+			console.log( "***********************" );
+			console.log( test.name );
 			console.log( "Difference: " + diff );
 			if( diff > 0 ) {
 				console.log( "NOT MATCHED" );
-				img_html = img_html.replace( "[" + test.file + "]",
-					"<span class='error'>NOT MATCHED - Difference: " + diff_rounded + "</span>" );
-				mis_match_count += 1;
+				g_ImgHtml[ i ] = g_ImgHtml[ i ].replace( "[" + test.file + "]",
+					"<span class='error'>NOT MATCHED - Difference: " + diffRounded + "</span>"
+				);
+				g_Errors.push( {
+					"test": test,
+					"type": "Not Matched"
+				} );
+				g_MismatchCount += 1;
 			} else {
-				img_html = img_html.replace( "[" + test.file + "]",
+				g_ImgHtml[ i ] = g_ImgHtml[ i ].replace( "[" + test.file + "]",
 					"<span class='good'>MATCHED</span>" );
 			}
-			next_test( i + 1 );
+			updateCounts();
+			//run_test( i + 1 );
 		}
 
 		if( err ) {
-			console.log( "error", err );
-			//return;
+			g_StrLog += "Error: " + err + "\n";
 		}
 		if( data ) {
 			console.log( data );
 		}
 		if( stderr ) {
-			console.log( "stderr", stderr );
-			//return;
+			g_StrLog += "StdError: " + stderr + "\n";
 		}
 
 		//console.log(test);
 
 		//Load Image Files
 		if( test.new_img_file ) {
-			files_loaded = 0;
-			img1 = fs.createReadStream( test.img_file ).pipe( new png() ).on( "parsed", parsed );
-			img2 = fs.createReadStream( test.new_img_file ).pipe( new png() ).on( "parsed", parsed );
+			filesLoaded = 0;
+			img1 = FS.createReadStream( test.img_file ).pipe( new PNG() ).on( "parsed", parsed );
+			img2 = FS.createReadStream( test.new_img_file ).pipe( new PNG() ).on( "parsed", parsed );
 		} else {
 			console.log( "IMAGE NOT VERIFIED" );
-			img_html = img_html.replace( "[" + test.file + "]", "<span class='neutral'>Not Verified</span>" );
-			new_tests_count += 1;
-			next_test( i + 1 );
+			g_ImgHtml[ i ] = g_ImgHtml[ i ].replace( "[" + test.file + "]", 
+				"<span class='neutral'>Not Verified</span>"
+			);
+			g_Errors.push( {
+				"test": test,
+				"type": "Not Verified"
+			} );
+			g_NewTestsCount += 1;
+			updateCounts();
 		}
 
 	} );
 
 }
 
+function updateCounts() {
+	g_totalTestsParsedCount += 1;
+	if( g_totalTestsParsedCount === g_totalTestsCount ) {
+		writeFinalHtml();
+	}
+}
+
+function writeFinalHtml() {
+
+	//Update the stats
+	let statsHtml = "<div id='stats'></div>";
+	if( g_MismatchCount === 0 && g_NewTestsCount === 0 ) {
+		statsHtml = "\n\t\t\t<span class='good'>All images match!</span>\n\t\t";
+	} else {
+		statsHtml += "\n\t\t\t<span class='error'>Discrepancies Found</span>";
+		if( g_MismatchCount ) {
+			statsHtml += "\n\t\t\t<span class='error'>" + g_MismatchCount +
+				" mismatched images.</span>";
+			if( ! g_NewTestsCount ) {
+				statsHtml += "\n\t\t";
+			}
+		}
+		if( g_NewTestsCount ) {
+			statsHtml += "\n\t\t\t<span class='neutral'>" + g_NewTestsCount +
+				" new images not verified.</span>\n\t\t";
+		}
+		if( g_Errors.length > 0 ) {
+			statsHtml += "\n\t\t\t<ol>";
+		}
+		for( let i = 0; i < g_Errors.length; i++ ) {
+			statsHtml += "\n\t\t\t\t<li><a href='#" + TEST_HTML_ID + g_Errors[ i ].test.id + "'>" + 
+				g_Errors[ i ].test.name + " - " + g_Errors[ i ].type + "</a></li>";
+		}
+		if( g_Errors.length > 0 ) {
+			statsHtml += "\n\t\t\t</ol>";
+		}
+	}
+	g_StrHtml = g_StrHtml.replace( "[TEST-STATS]", statsHtml );
+
+	//Update the index_html
+	g_StrHtml = g_StrHtml.replace( "[WEB-TESTS]", g_ImgHtml.join( "" ) );
+
+	//Write the index.html file
+	FS.writeFile( LOG_FILE, g_StrLog, function () {} );
+
+	FS.writeFile( INDEX_FILE, g_StrHtml, function () {
+		console.log( "Tests completed" );
+	} );
+
+	//Set the command to startup chrome and point to the home page
+	let cmdStr = BROWSER + " " + HOME;
+
+	//Launch Chrome with link to test file
+	CMD.get( cmdStr, function ( err, data, stderr ) {
+		if( err ) {
+			g_StrLog += "Error: " + err + "\n";
+		}
+		if( data ) {
+			console.log( data );
+		}
+		if( stderr ) {
+			g_StrLog += "Error: " + stderr + "\n";
+		}
+	} );
+}
+
+function getTestInfo( filename ) {
+	let text = FS.readFileSync( TESTS_FOLDER + "/" + filename ).toString();
+	let tomlText = text.substring( text.indexOf( "[[TOML_START]]" ) + 14, text.indexOf( "[[TOML_END]]" ) ).replace( /\t/g, "" );
+	let data = TOML.parse( tomlText );
+	return data;
+}
+
+function isHtmlFile( filename ) {
+	let parts = filename.split( "." );
+	if( parts.length < 2 ) {
+		return false;
+	}
+	return parts[ 1 ] === "html";
+}
+
+
 function compare_images( img1, img2 ) {
-	"use strict";
 
 	var x, y, i, diff, p_diff, width, height;
 
