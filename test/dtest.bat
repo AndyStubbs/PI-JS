dc4402d5d941f67324343d11531d7549aa1da736
Andy Stubbs <astubbs50@gmail.com>
2020-07-28 16:30:52 -0700

    Update print test

--- test/dtest.bat
+++ test/dtest.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Deleting test\tests\screenshots\offkey_02.png
-del C:\src\qbs\test\tests\screenshots\offkey_02.png
-echo Renaming C:\src\qbs\test\tests\screenshots\offkey_02_new.png
-move C:\src\qbs\test\tests\screenshots\offkey_02_new.png C:\src\qbs\test\tests\screenshots\offkey_02.png
+echo Deleting test\tests\screenshots\print_01.png
+del C:\src\qbs\test\tests\screenshots\print_01.png
+echo Renaming C:\src\qbs\test\tests\screenshots\print_01_new.png
+move C:\src\qbs\test\tests\screenshots\print_01_new.png C:\src\qbs\test\tests\screenshots\print_01.png
