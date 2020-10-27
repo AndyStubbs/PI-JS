73deca41009f2391f97b2fb225e5fc250d91b4e4
Andy Stubbs <astubbs50@gmail.com>
2020-10-27 10:58:49 -0700

    Added new test for input

--- test/dtest2.bat
+++ test/dtest2.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Deleting C:\src\qbs\test\tests\screenshots\get_01.png
-del C:\src\qbs\test\tests\screenshots\get_01_new.png
-echo Deleting C:\src\qbs\test\tests\screenshots\get_01_new.png
-del C:\src\qbs\test\tests\screenshots\get_01.png
+echo Deleting C:\src\qbs\test\tests\screenshots\input_04.png
+del C:\src\qbs\test\tests\screenshots\input_04_new.png
+echo Deleting C:\src\qbs\test\tests\screenshots\input_04_new.png
+del C:\src\qbs\test\tests\screenshots\input_04.png
